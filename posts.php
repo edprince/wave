a560a8052a0a8fa41a3e76f80bb5f64e1b359123
ed <edward_prince@hotmail.com>
2015-08-08 18:21:54 +0100

    files created

---
+++ posts.php
@@ -0,0 +1,11 @@
+<?php require('includes/header.html'); ?>
+<?php require('includes/nav.html'); ?>
+
+<div class='post'>
+  <h2>My blog title</h2>
+  <p class='post-content'>This is the blog post</p>
+  <p class='author'>Ed Prince</p>
+  <p class='date'>19.03.15</p>
+</div>
+
+<?php require('includes/footer.html'); ?>
